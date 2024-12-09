36b42e28ba2a06d38e7ee0a6f6057c421ee1348d
Lok Leoi <lyulelok@gmail.com>
2024-12-08 21:04:35 -0500

    Initial Commit

---
+++ BigEmoji/BigEmojiApp.swift
@@ -0,0 +1,17 @@
+//
+//  BigEmojiApp.swift
+//  BigEmoji
+//
+//  Created by Lok Lyu on 12/8/24.
+//
+
+import SwiftUI
+
+@main
+struct BigEmojiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
